6d0914424c0a4bd61b50d0a715cd356c7f7aec43
RPXIC <rpc2works@gmail.com>
2020-01-21 15:44:49 +0100

    add more chl

--- staff/ruben-ponce/arrays/isarray.js
+++ staff/ruben-ponce/arrays/isarray.js
@@ -1,5 +1,7 @@
 'use strict'
 var a = [1, 2, 3, 4];
 
-if (a.prototype.constructor.name === 'Array') return true;
-else return false;
+function isarray() {
+    if (a.__proto__.constructor.name === "Array") return true;
+    else return false;
+}
