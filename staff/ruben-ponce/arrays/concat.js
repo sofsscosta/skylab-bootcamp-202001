efd6b5c612d99557b173966531297a653ce8fd7d
RPXIC <rpc2works@gmail.com>
2020-01-21 09:50:54 +0100

    a

--- staff/ruben-ponce/arrays/concat.js
+++ staff/ruben-ponce/arrays/concat.js
@@ -3,14 +3,11 @@ var a = [1, 2, 3, 4];
 var b = [5, 6, 7, 8];
 
 function concat(arr1, arr2) {
-    var c = [];
+    
     for (var i = 0; i < arr1.length; i++) {
-        c[i] = arr1[i];
+        a[a.length] = arr2[i];
     }
-    for (var j = 0; j < arr2.length; j++) {
-        c[i+j] = arr2[j];
-    }
-    return c
+    return a;
 }
 
 //arguments.length-2
