c0a2652bf94477fc4864d9f8e7465da05f15dd92
abousiyd <59477326+abousiyd@users.noreply.github.com>
2020-01-25 19:25:22 +0100

    add test

--- staff/abdou-bousiyd/arrays/findIndex/findIndex.test.js
+++ staff/abdou-bousiyd/arrays/findIndex/findIndex.test.js
@@ -1,17 +1,55 @@
 console.log('test findIndex');
 
-var a = ['z', 'b', 'g', 'y'];
-var callback = element => element === 'b'
-var returnValue = findIndex(a, callback)
 
+(function() {
+    'use strict';
 
-console.log('should return the correct position of element');
-console.assert(returnValue === 1, 'should return position 1');
+    console.log('TEST includes');
+    console.log('should succeed on searching inside an array and returning the index of the element that satisfies the condition, if exists');
 
-var callback2 = element => element === 'p'
-var returnValue2 = findIndex(a, callback2)
+    function assert(assertion, message) {
+        if (!assertion) throw new Error('Assertion failed: ' + message);
+    }
 
-console.log('should return -1 when element does not exist');
-console.assert(returnValue2 === -1, 'should return position -1');
+    (function() {
+        console.log('should return the correct position of element');
+            (function () {
+                var a = ['z', 'b', 'g', 'y'];
+                var callback = element => element === 'b'
+                var returnValue = findIndex(a, callback)
 
+                console.assert(returnValue === 1, 'should return position 1');
+            })();
+        console.log('%c Done %s', 'color: green', '✔');
+    }());
 
+    (function() {
+        console.log('should return -1 when element does not exist');
+        (function () {
+            var a = ['z', 'b', 'g', 'y'];
+            var callback = element => element === 'f'
+            var returnValue = findIndex(a, callback)
+
+            console.assert(returnValue === -1, 'should return position -1');
+        })();
+    console.log('%c Done %s', 'color: green', '✔');
+    }());
+
+    (function() {
+          console.log('should fail with non-array input (undefined)');
+        try{
+            var a
+            var callback = function (num) { return num < 10; };
+            var _error;
+            (function () {
+                findIndex(a, callback)
+                console.assert(callback === undefined, 'should return array is undefained');
+            })();
+        }catch(error) {
+            _error = error;
+        }
+        if (!_error) throw Error ('should be failed')
+        if (_error.message === a + ' is not an array') throw Error('error message is not correct: ' + _error.message);
+         console.log('%c Done %s', 'color: green', '✔');
+    }());
+}())
