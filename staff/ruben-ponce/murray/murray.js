101649f2ded09f8deddb2a97c673da94c5ebe6ef
RPXIC <rpc2works@gmail.com>
2020-01-27 14:48:21 +0100

    1

--- staff/ruben-ponce/murray/murray.js
+++ staff/ruben-ponce/murray/murray.js
@@ -120,3 +120,19 @@ var count = 0;
     }
     return c
 };
+
+Murray.prototype.slice = function () {
+
+};
+
+// var a = [1, 2, 3, 4, 5, 6];
+
+// function slice(arr, index, fIndex = arr.length) {
+//     var b = [];
+
+//     for (var i = index; i < fIndex; i++) {
+//         b[b.length] = arr[i];
+//     }
+//     debugger
+//     return b;
+// }
