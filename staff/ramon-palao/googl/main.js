7a344810ee4401c339b990e5196ed63d3a62b8a1
ramon <ramon.palao@gmail.com>
2020-01-28 20:29:00 +0100

    add googl template started

--- staff/ramon-palao/googl/main.js
+++ staff/ramon-palao/googl/main.js
@@ -4,9 +4,25 @@ form.addEventListener("submit", function(event){
     event.preventDefault();
 
     var query = this.query.value;
-
+    var ul = document.createElement("ul");
     googl(query, function(results){
-        console.log(results);
+        //console.log(results);
+        for(var i = 0; i < results.length; i++){
+            var result = results[i];
+
+            var li = document.createElement("li");
+            var h3 = document.createElement("h3");
+            var p = document.createElement("p");
+
+            h3.innerHTML = result.title;
+            p.innerHTML = result.description;
+
+            li.append(h3, p);
+            ul.append(li);
+        }
+        var div = document.getElementById("div");
+        div.innerHTML = "";
+        div.append(ul);
     });
 });
     
