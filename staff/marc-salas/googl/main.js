b1e767e041f9d3300cf7c9c847ab4756abc497e2
MarcSalasTorras <marc.salas.torras@gmail.com>
2020-01-28 18:52:32 +0100

    logger

--- staff/marc-salas/googl/main.js
+++ staff/marc-salas/googl/main.js
@@ -1,3 +1,31 @@
+var user = [];
+
+var reg = createRegister('.register', function(name,surName,userName,password){
+    var person = {}
+    person.name = name;
+    person.userName = userName;
+    person.surName = surName;
+    person.password = password;
+    if (person.name.length > 0 && person.surName.length > 0 && person.userName.length > 0 && person.password.length > 0 ){
+        user.push(person);
+        register.classList.toggle('register--hide');
+        login.classList.toggle('login--hide');
+    }else {
+        alert('Completa todos los campos!')
+    };
+
+});
+
+var login = createLogin('.login', function(username, password) {
+    if (username === 'pepito' && password === '123') {
+        search.classList.toggle('search--hide');
+        login.classList.toggle('login--hide');
+    } else alert('you cannot get in :P');
+});
+
+
+
+
 var from = document.querySelector('form');
 
 from.addEventListener('submit', function(event){
@@ -21,4 +49,5 @@ from.addEventListener('submit', function(event){
     var main = document.querySelector('#main');
     main.innerHTML = " ";
     main.append(ul);
-}); 
+});
+
