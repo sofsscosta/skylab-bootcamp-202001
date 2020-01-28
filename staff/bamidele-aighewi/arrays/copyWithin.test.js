7fa58ad1c4ffea4353368f004bcc6b899a85cf24
Eric Aig <>
2020-01-28 10:03:48 +0100

    implementing copyWithin test

---
+++ staff/bamidele-aighewi/arrays/copyWithin.test.js
@@ -0,0 +1,7 @@
+describe('copyWithin', function(){
+    it('should initialize method', function () {
+        assert(typeof copyWithin === 'function', 'Function method expected got ' + (typeof copyWithin) + ' instead')
+    });
+
+    // it('')
+})
