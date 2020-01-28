4f77e22cf4cbac3da086a8d802d3421db6a45569
Federico Lacabaratz <federicolacabaratz@MacBook-Pro-de-Federico.local>
2020-01-28 17:14:03 +0100

    add update to googl challenge

--- staff/federico-lacabaratz/googl/googl.js
+++ staff/federico-lacabaratz/googl/googl.js
@@ -27,18 +27,19 @@ function googl(query, callback) {
                     result.title = title.innerText;
 
                     var rating = item.querySelector('.slp.f');
-                    var description = item.querySelector('span.st');
-
+                    
                     if (rating) {
                         result.rating = rating.innerText;
                     }
+                    
+                    var description = item.querySelector('.st');
 
-                    if (description) {
-                        result.description = description.innerText;
-                    }
+                    result.description = description.innerText;
+                    var link = item.querySelector('.iUh30')
+                    result.link = link.innerText
+                    results.push(result);
                 }
 
-                results.push(result);
             };
 
             callback(results);
@@ -47,3 +48,43 @@ function googl(query, callback) {
 
     xhr.send();
 };
+
+function googl(query, callback) {
+    if (callback instanceof Array) throw new TypeError(callback.constructor.name + ' is not a function');
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function');
+    var xhr = new XMLHttpRequest
+    xhr.open('GET', 'https://skylabcoders.herokuapp.com/proxy?url=https://www.google.com/search?q=' + query)
+    xhr.onreadystatechange = function (res) {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.readyState)
+            var doc = new DOMParser().parseFromString(this.responseText, 'text/html')
+            var items = doc.querySelectorAll('div.g')
+            var results = []
+            for (var i = 0; i < items.length; i++) {
+                var item = items[i]
+                var title = item.querySelector('h3.LC20lb')
+                if (title) {
+                    var result = {}
+                    result.title = title.innerText
+                    var rating = item.querySelector('.slp.f')
+                    if (rating) {
+                        result.rating = rating.innerText
+                    }
+                    var description = item.querySelector('.st')
+                    result.description = description.innerText
+                    var link = item.querySelector('.iUh30')
+                    result.link = link.innerText
+                    results.push(result)
+                }
+                else ''
+            }
+            callback(results)
+        }
+    }
+    xhr.send()
+}
+
+
+
+
+
