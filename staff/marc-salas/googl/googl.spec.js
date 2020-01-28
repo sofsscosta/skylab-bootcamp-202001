41ac777e02057b7a99347ecb94ba00ca344d8012
MarcSalasTorras <marc.salas.torras@gmail.com>
2020-01-28 12:54:20 +0100

    googl list.

--- staff/marc-salas/googl/googl.spec.js
+++ staff/marc-salas/googl/googl.spec.js
@@ -1,16 +1,23 @@
-googl('pepito', function(results) { 
-    results.forEach(function(result) { 
-        console.assert(result.title,'should every result have s description');
-        console.log(result) 
+describe('googl aplication',function(){
+
+    it('should all the results have a title', function(){
+        googl('pepito', function(results) { 
+            results.forEach(function(result) { 
+                expect(result.title.length > 0).toBe(true)
+            })
+        });
     })
+
 });
 
-(function(){
-    try{
-        googl('pepito', 1);
-    } catch(error){
-       var _error = error;
-    }
-    console.assert(_error instanceof TypeError, 'Wrong error data type');
 
-})();
+
+// (function(){
+//     try{
+//         googl('pepito', 1);
+//     } catch(error){
+//        var _error = error;
+//     }
+//     console.assert(_error instanceof TypeError, 'Wrong error data type');
+
+// })();
