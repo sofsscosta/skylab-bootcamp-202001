9868c8f38a208134be59ec86da83ea7f1b90db18
Federico Lacabaratz <federicolacabaratz@MacBook-Pro-de-Federico.local>
2020-01-28 21:12:19 +0100

    add update to googl challenge

--- staff/federico-lacabaratz/googl/main.js
+++ staff/federico-lacabaratz/googl/main.js
@@ -1,14 +1,34 @@
-var users = []; // ej: user => { name, surname, username, password }
+var search = createSearch('.search', function(query) {
 
-var search = createSearch('.search', function (query) {
-    googl(query, function (results) {
-        createResults('.results', results);
-    });
-});
-
-var login = createLogin('.login', function(username, password) {
-    if (username === 'pepito' && password === '123') {
+    googl(query, function(results) {
+      createResults('.results', results)
+    })
+  }) 
+  
+  var login = createLogin('.login', function(username, password) {
+    
+    var userFound = users.filter(function(user) {
+        return username === user.username && password === user.password
+    })
+  
+    if (userFound.length > 0) {
         search.classList.toggle('search--hide');
         login.classList.toggle('login--hide');
+        
+        var nav = document.querySelector('.nav');
+        nav.classList.toggle("nav--hide")
+        
     } else alert('you cannot get in :P');
 });
+
+
+createRegister('.register', function() {
+    
+    var register = document.querySelector('.register')
+    register.classList.add('register--hide');
+    login.classList.toggle('login--hide')  
+    
+  });
+  
+  
+  createNavbar()
