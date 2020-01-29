ccde2ed0cb8d11f7d45da3ecd0e564a6c99040ab
ramon <ramon.palao@gmail.com>
2020-01-29 18:47:41 +0100

    adding new files of googl

--- staff/ramon-palao/googl/main.js
+++ staff/ramon-palao/googl/main.js
@@ -79,21 +79,6 @@ createRegister(".register", function(name, surname, username, password){
 
 })
 
-function createLogin(selector, callback) {
-    var login = document.querySelector(selector);
-
-    login.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        var username = this.username.value;
-        var password = this.password.value;
-
-        callback(username, password);
-    });
-
-    return login;
-}
-
 var search = document.querySelector(".header__results");
 
 createLogin(".login", function(username, password){
@@ -110,60 +95,6 @@ createLogin(".login", function(username, password){
         alert("You cannot get in");
     }
 })
-
-    
-
-
-function createSearch(selector, callback) {
-    var search = document.querySelector(selector);
-
-    // search.onsubmit = function (event) {
-    search.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        var query = this.query.value;
-
-        callback(query);
-        // };
-    });
-
-    return search;
-}
-
-function createResults(selector, results) {
-    var list = document.querySelector(selector);
-
-    list.innerHTML = '';
-
-    results.forEach(function (result) {
-        var item = document.createElement('li');
-
-        var title = document.createElement('h3');
-        title.innerText = result.title;
-
-        var link = document.createElement('a');
-        link.target = '_blank';
-        link.href = result.link;
-
-        link.append(title);
-
-        item.append(link);
-
-        if (result.rating) {
-            var rating = document.createElement('span');
-            rating.innerText = result.rating;
-
-            item.append(rating);
-        }
-
-        var description = document.createElement('p');
-        description.innerText = result.description;
-
-        item.append(description);
-
-        list.append(item);
-    });
-}
 
 createSearch(".search__form", function(query){
     googl(query, function(results){
