3632a8a5f4bd592612798ad58c663857b5ff39f0
MarcSalasTorras <marc.salas.torras@gmail.com>
2020-01-29 12:43:58 +0100

    sass

--- staff/marc-salas/googl/main.js
+++ staff/marc-salas/googl/main.js
@@ -38,27 +38,3 @@ showSelection('.landing', '.toRegister', '.register');
 
 
 
-// var form = document.querySelector('form');
-
-// form.addEventListener('submit', function(event){
-//     event.preventDefault();
-
-//     var query = this.query.value;
-    
-//     var ul = document.createElement('ul');
-//     googl(query, function(results){
-//         for (var i =0; i < results.length; i++){
-//             var result = results[i];
-//             var li = document.createElement('li');
-//             var h3 = document.createElement('h3');
-//             var p = document.createElement('p');
-//             p.innerHTML = result.description;
-//             h3.innerHTML = result.title;
-//             li.append(h3,p);
-//             ul.append(li)
-//         }
-//     });
-//     var main = document.querySelector('#main');
-//     main.innerHTML = " ";
-//     main.append(ul);
-// });
