6c7b019d332b7c1de29f5cd294d36ee8903f8e66
MarcSalasTorras <marc.salas.torras@gmail.com>
2020-01-29 14:43:04 +0100

    start implementation folder 3

---
+++ staff/marc-salas/googl/call.js
@@ -0,0 +1,16 @@
+function call(url, callback){
+    if (!( typeof url === 'string')) throw new TypeError('url should be a string');
+    if (!( typeof callback === 'function')) throw new TypeError('callback should be a function');
+
+    var xhr = new XMLHttpRequest;
+    
+    xhr.open('GET', url);
+
+    xhr.onreadystatechange = function(){
+        if (this.readyState === 4)
+        callback({
+            content: this.responseText,
+            status: this.status
+        });
+    }
+}
