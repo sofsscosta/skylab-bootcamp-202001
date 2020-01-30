f01afea537ea8f72ccc0beef486b18fb6ac2f13d
momorales <monimoralesr77@gmail.com>
2020-01-30 01:23:35 +0100

    add last part of Manu

--- staff/monica-morales/googl/main.js
+++ staff/monica-morales/googl/main.js
@@ -1,6 +1,6 @@
-var users = []; // ej: user => { name, surname, username, password }
+// var users = []; // ej: user => { name, surname, username, password }
 
-var search = createSearch('.search', function (query) {
+var _googl = createSearch('.search', function (query) {
     googl(query, function (results) {
         createResults('.results', results);
     });
@@ -10,11 +10,11 @@ var login = createLogin('.login', function(username, password) {
         return element.username === username && element.password === password;
     });
     if (user.length === 1) {
-        search.classList.toggle('search--hide');
+        _googl.classList.toggle('search--hide');
         login.classList.toggle('login--hide');
     } else alert('you cannot get in :P');
 });
-var register = createRegiter('.register', function(name,surname,username,password){
+var register = createRegister('.register', function(name,surname,username,password){
     var user = {};
     user.name = name;
     user.surname = surname;
