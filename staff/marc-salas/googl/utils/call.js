51442e291db52dfb27c443ec303450a003d9bf81
MarcSalasTorras <marc.salas.torras@gmail.com>
2020-01-31 11:59:40 +0100

    spec

--- staff/marc-salas/googl/utils/call.js
+++ staff/marc-salas/googl/utils/call.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
+
 function call(url, callback){
     if (!( typeof url === 'string')) throw new TypeError('url should be a string');
     if (!( typeof callback === 'function')) throw new TypeError('callback should be a function');
