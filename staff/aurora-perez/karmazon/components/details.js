66d5ddddd902e5f9650b91e6c956c2f3c6fe722a
Aurora Pérez Alonso <aurora.perez1993@gmail.com>
2020-02-02 23:06:03 +0100

    CHL karmazon

--- staff/aurora-perez/karmazon/components/details.js
+++ staff/aurora-perez/karmazon/components/details.js
@@ -10,13 +10,13 @@ function Details (detailInfo) {
     details.innerHTML = '<button class="details__button">BACK</button>'
         .concat('<div class="details__title-img"><h2>' + detailInfo.id +': '+detailInfo.name+'</h2>')
         .concat('<img src="' + detailInfo.image + '"></div>')
-        .concat('<div class="details__content"><p class ="details__year">YEAR: ' + detailInfo.year + '</p>')
-        .concat('<p class ="details__content-color">COLOR: ' + detailInfo.color.toProperCase() + '</p>')
-        .concat('<p class ="details__content-cmaker>MAKER: ' + detailInfo.maker.toProperCase() + '</p>')
-        .concat('<p class ="details__content-ccollection>COLLECTION: ' + detailInfo.collection.toProperCase() + '</p>')
-        .concat('<p class ="details__content-cstyle>STYLE: ' + detailInfo.style.toProperCase() + '</p>')
-        .concat('<p class ="details__content-cdescription>DESCRIPTION: ' + detailInfo.description + '<p>')
-        .concat('<span>PRICE: $ ' + detailInfo.price + ' </span></div>');   
+        .concat('<div class="details__content"><p class ="details__year"><b>YEAR: </b>' + detailInfo.year + '</p>')
+        .concat('<p class ="details__content-color"><b>COLOR:</b> ' + detailInfo.color.toProperCase() + '</p>')
+        .concat('<p class ="details__content-cmaker><b>MAKER: </b>' + detailInfo.maker.toProperCase() + '</p>')
+        .concat('<p class ="details__content-ccollection><b>COLLECTION: </b>' + detailInfo.collection.toProperCase() + '</p>')
+        .concat('<p class ="details__content-cstyle><b>STYLE: </b>' + detailInfo.style.toProperCase() + '</p>')
+        .concat('<p class ="details__content-cdescription><b>DESCRIPTION: </b>' + detailInfo.description + '<p></div>')
+        .concat('<span class ="details__price"><b>PRICE: $ </b>' + detailInfo.price + ' </span>');   
 
 }
 
--- staff/aurora-perez/karmazon/data.js
+++ staff/aurora-perez/karmazon/data.js
@@ -1,4 +1,3 @@
 var users = []; 
 
-users.push({ name: 'Aurora', surname: 'Perez', username: 'Au', password: '123' },
-{name: 'holi', surname: 'F', username: '', password: ''});
+users.push({ name: 'Aurora', surname: 'Perez', username: 'Au', password: '123' });
