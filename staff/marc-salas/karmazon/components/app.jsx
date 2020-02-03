87d559f83b46cbb838102e29ad8931551ebc4123
MarcSalasTorras <marc.salas.torras@gmail.com>
2020-02-03 20:57:11 +0100

    start react not inplemented well

---
+++ staff/marc-salas/karmazon/components/app.jsx
@@ -0,0 +1,27 @@
+const IT = '🎈🤡'
+
+const { Component } = React
+
+class App extends Component {
+    constructor() {
+        super()
+
+        this.state = { loggedIn: !false }
+    }
+
+    render() {
+        return <main>
+            <h1>{this.props.title}</h1>
+            {!this.state.loggedIn && <Login onSubmit={(username, password) => {
+                try {
+                    authenticate(username, password)
+
+                    this.setState({ loggedIn: true })
+                } catch (error) {
+                    //_login.showError(error.message + ' ' + IT)
+                }
+            }} />}
+        </main>
+    }
+}
+
