f648e2b13c2876cbd9c6e5e09d3db3685cb6c3a7
Federico Lacabaratz <federicolacabaratz@MacBook-Pro-de-Federico.local>
2020-02-06 13:37:37 +0100

    add updates to karmazon challenge

--- staff/federico-lacabaratz/karmazon/logic/user-update.js
+++ staff/federico-lacabaratz/karmazon/logic/user-update.js
@@ -1,3 +1,13 @@
-function userUpdate(){
+function userUpdate(token, oldUsername, newUsername,callback){
+    if(typeof oldUsername !== "string") throw new TypeError (`oldusername ${oldUsername} is not a string`)
+    if(typeof newUsername !== "string") throw new TypeError (`newusername ${newUsername} is not a string`)
+    if(typeof callback !== "function") throw new TypeError (`callback ${callback} is not a function`)
+
+call(`https://skylabcoders.herokuapp.com/api/v2/users`, {
+    method: "PATCH",
+    headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    },
+    body: 
     
-}
