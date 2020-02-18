2d1a40320d985df8816d1e3fd541087bdf03bc36
Manuel Barzi <manuelbarzi@gmail.com>
2020-02-18 14:45:17 +0100

    boost web-server

--- staff/manuel-barzi/web-server/index.js
+++ staff/manuel-barzi/web-server/index.js
@@ -1,1 +1,42 @@
-// TODO
+const net = require('net')
+const logger = require('./logger')
+const fs = require('fs')
+
+logger.info('starting server')
+
+const server = net.createServer(socket => {
+    logger.debug('setting encoding to utf8')
+    socket.setEncoding('utf8')
+
+    socket.on('data', request => {
+        logger.info(`request received ${request} from ${socket.remoteAddress}`)
+
+        const lines = request.split('\n')
+        let [, path] = lines[0].split(' ')
+
+        if (path === '/') path += 'index.html'
+
+        path = `.${path}`
+
+        fs.readFile(path, 'utf8', (error, content) => {
+            if (error) {
+                logger.error(error)
+
+                return socket.end(`HTTP/1.1 404 NOT FOUND
+Content-Type: text/html
+
+<h1>Not found</h1>`)
+            }
+
+            socket.end(`HTTP/1.1 200 OK
+Content-Type: text/html
+
+${content}
+`)
+        })
+    })
+
+    socket.on('error', error => logger.error(error))
+})
+
+server.listen(8080)
