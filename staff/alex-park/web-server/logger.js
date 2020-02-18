ad64b91fedc270059fff6f96ac7114a2b9c3f02f
Alex Park <alexparkvinyas@gmail.com>
2020-02-18 18:36:55 +0100

    sdfkj

--- staff/alex-park/web-server/logger.js
+++ staff/alex-park/web-server/logger.js
@@ -1,11 +1,15 @@
 function log(level, message) {
     const fs = require('fs')
-    fs.appendFile('server.log', `${level}: ${message}\n`, 'utf8', error => {
-        if (error) throw error
+    fs.writeFile('./server.log', `${level} (${new Date}): ${message}\n`, { encoding: 'utf8', flag: 'a' }, error => {
+        if (error) console.error(error)
     })
 }
 
 module.exports = {
+    __debugEnable__ = false,
+    setDebugEnable(enable) {
+        this.__debugEnable__ = enable
+    },
     debug(message) { log('DEBUG', message) },
     info(message) { log('INFO', message) },
     warn(message) { log('WARN', message) },
