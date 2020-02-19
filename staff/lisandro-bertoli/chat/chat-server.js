7117e2ed6acd35d286c25250859889b56066a974
Lisandro B <lisandro.bertoli@gmail.com>
2020-02-19 09:28:46 +0100

    revisionb

--- staff/lisandro-bertoli/chat/chat-server.js
+++ staff/lisandro-bertoli/chat/chat-server.js
@@ -26,47 +26,4 @@ const server = net.createServer(socket => {
     })
 })
 
-
-
 server.listen(8080)
-
-
-// if (typeof module !== 'undefined')
-//     module.exports = chatServer
-
-
-
-
-
-
-
-// const net = require('net')
-
-// const clients = {}
-
-// const server = net.createServer(socket => {
-//     socket.on('data', chunk => {
-
-//         const userName = chunk.toString().split(':')[0]
-
-//         console.log(chunk.toString())
-
-//         clients[userName] = socket
-
-//         for (client in clients) {
-//             clients[client].write(chunk.toString())
-//         }
-
-//     })
-
-// })
-
-
-// server.on('error', error => {
-//     throw error
-// })
-
-
-// server.listen(8080, () => {
-//     console.log('server listening')
-// })
