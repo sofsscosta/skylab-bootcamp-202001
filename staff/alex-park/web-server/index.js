814c63080cae411e9bbae063514c484da922b8f8
Alex Park <alexparkvinyas@gmail.com>
2020-02-19 18:09:02 +0100

    fixed index

--- staff/alex-park/web-server/index.js
+++ staff/alex-park/web-server/index.js
@@ -3,17 +3,17 @@ const logger = require('./utils/logger')
 const fs = require('fs')
 
 const { argv: [, , port = 8080] } = process
-
 // logger.setDebugEnabled(true)
 
 const app = express()
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.send(req)
-})
+app.get('/', (req, res) => { 
+    debugger
+    logger.info(`New request: ${req}`); 
+    res.send(req) })
 
 logger.debug('starting server')
 
-const server = app.listen(port, () => { logger.info(`Connected to server on port ${port}`) })
+const server = app.listen(port, () => { logger.info(`¡¡¡Connected to server on port ${port}!!!!!!!!!!!!!!`) })
