d030bb5215932831e77a6cd454d40b250310259c
Alex Park <alexparkvinyas@gmail.com>
2020-02-19 19:04:33 +0100

    ready for PR

--- staff/alex-park/web-server/index.js
+++ staff/alex-park/web-server/index.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const logger = require('./utils/logger')
-const fs = require('fs')
+const path = require('path')
+const loggerMidWare = require('./utils/looger-mid-ware')
 
 const { argv: [, , port = 8080] } = process
-// logger.setDebugEnabled(true)
+
+logger.level = logger.INFO
+logger.path = path.join(__dirname, 'server.log')
 
 const app = express()
 
-app.use(express.static('public'))
+app.use(loggerMidWare)
 
-app.get('/', (req, res) => { 
-    debugger
-    logger.info(`New request: ${req}`); 
-    res.send(req) })
+app.use(express.static(path.join(__dirname, 'public')))
 
 logger.debug('starting server')
+app.listen(port, () => { logger.info(`Connected to server on port ${port}`) })
 
-const server = app.listen(port, () => { logger.info(`¡¡¡Connected to server on port ${port}!!!!!!!!!!!!!!`) })
+process.on('SIGINT', () => { logger.warn(`server abruptly stopped`); process.exit(0) })
