3e0b5a8cbee9eea0c6f496a889f17a7a5a0d0460
Sofia Costa <58744044+sofsscosta@users.noreply.github.com>
2020-02-19 10:57:07 +0100

    small changes

--- staff/sofia-costa/web-server/http.js
+++ staff/sofia-costa/web-server/http.js
@@ -6,24 +6,25 @@ const { argv: [, , port = 8000] } = process
 
 const server = http.createServer((req, res) => {
 
-    logger.info(`request from ${req.socket.remoteAdress} : ${req.url}`)
+    logger.info(`request from ${req.socket.remoteAddress} : ${req.url}`)
     
     const rs = fs.createReadStream(`.${req.url === '/' ? '/index.html' : req.url}`)
     
     if(req.url !== 'favicon.ico') {
         rs.on('data', body => {
+            res.writeHead(200, { 'Content-Type' : 'text/html' })
             res.end(body)
         })
     
         rs.on('error', error => {
             logger.warn(error)
-            res.writeHead(404)
+            res.writeHead(404, { 'Content-Type' : 'text/html' })
             res.end('<h1>NOT FOUND</h1>')
         })
     }
     else {
         logger.warn(error)
-        res.writeHead(404)
+        res.writeHead(404, { 'Content-Type' : 'text/html' })
         res.end('<h1>NOT FOUND</h1>')
     }
 })
