ac1606828402dbebf45089b57e73097ca55ef280
Carlos Matilla <carlosmatilla@MacBook-Pro-de-Carlos-2.local>
2020-02-21 10:40:49 +0100

    add karmazon

---
+++ staff/carlos-matilla/karmazon-es6/components/detail.jsx
@@ -0,0 +1,17 @@
+function Detail({vehicle:{name, price, image}}){
+    
+    return <li>
+        
+        <h3>{name}</h3>
+        <img src={image} />
+        <span>{price}</span>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <a href=""></a>
+        <img/>
+        <p></p>
+        <a href=""></a>
+    </li>
+}
---
+++ staff/carlos-matilla/karmazon-es6/logic/register.spec.js
@@ -0,0 +1,20 @@
+describe('authenticate', () => {
+    let user
+
+    beforeEach(() => {
+        users.length = 0
+
+        user = {
+            name: 'name-' + Math.random(),
+            surname: 'surname-' + Math.random(),
+            username: 'username-' + Math.random(),
+            password: 'password-' + Math.random()
+        }
+    })
+
+    it ('shoud authenticate an user', (done=>{
+
+        
+
+    }))
+})
