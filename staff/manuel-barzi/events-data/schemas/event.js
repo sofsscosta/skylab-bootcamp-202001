2824a5afbf25ccb091203556490861f5ead29a4d
Manuel Barzi <manuelbarzi@gmail.com>
2020-03-05 17:32:43 +0100

    add embed credit card to user model

--- staff/manuel-barzi/events-data/schemas/event.js
+++ staff/manuel-barzi/events-data/schemas/event.js
@@ -7,5 +7,5 @@ module.exports = new Schema({
     location: { type: String, required: true },
     publisher: { type: ObjectId, required: true, ref: 'User' },
     created: { type: Date, required: true, default: Date.now },
-    subscribed: { type: [ObjectId], ref: 'User' }
+    subscribed: [{ type: ObjectId, ref: 'User' }]
 })
