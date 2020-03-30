afe5347a0ef42729c55a2cd90fa2a23009a727c0
Sofia Costa <58744044+sofsscosta@users.noreply.github.com>
2020-03-30 15:12:14 +0200

    updated readme

--- staff/sofia-costa/hoort/hoort-logic/is-logged-in.js
+++
@@ -1,7 +0,0 @@
-const context = require('./context')
-
-module.exports = async function () {
-    const token = await this.storage.getItem('token')
-
-    return !!token
-}.bind(context)
--- staff/sofia-costa/hoort/hoort-logic/logout.js
+++
@@ -1,5 +0,0 @@
-const context = require('./context')
-
-module.exports = function () {
-    return this.storage.removeItem('token')
-}.bind(context)
